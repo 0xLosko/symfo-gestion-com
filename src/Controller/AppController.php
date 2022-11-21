0efa19f0c6a9a521491f1c8356f0424f2ba3fa85
Loris Daca <losko@MacBook-Pro-de-Loris.local>
2022-11-21 14:08:12 +0100

    clear using

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,13 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-
-use App\Controller\Article;
-use Symfony\Component\HttpFoundation\Request;
 
 class AppController extends AbstractController
 {
